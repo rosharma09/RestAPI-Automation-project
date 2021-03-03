789dd1d95b6760c17734a26afbb6300ea001ddf7
rosharma09 <56192256+rosharma09@users.noreply.github.com>
2021-03-03 23:54:15 +0530

    Initial commit to the rest assured project

---
+++ src/test/java/testcases/GetUserAPI.java
@@ -0,0 +1,30 @@
+package testcases;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Title;
+
+@RunWith(SerenityRunner.class)
+public class GetUserAPI {
+	
+	
+	@BeforeClass
+	public static void init() {
+		RestAssured.baseURI = "https://reqres.in";
+		RestAssured.basePath = "/api/users";
+	}
+	
+	@Title(value = "Check the status code")
+	@Test
+	public void checkStatCode() {
+		Response httpResponse = SerenityRest.given().when().get("2");
+		httpResponse.then().statusCode(200);
+	}
+
+}
